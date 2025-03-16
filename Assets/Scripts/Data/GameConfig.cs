5b2b46d82277a22c62a87bb3e4f88fa0a247cabf
HungVu <hung.vutrung19@gmail.com>
2025-03-16 20:01:05 +0700

    Change Tomato tree spawn time

--- Assets/Scripts/Data/GameConfig.cs
+++ Assets/Scripts/Data/GameConfig.cs
@@ -5,7 +5,7 @@ namespace Data
     [CreateAssetMenu(fileName = "GameConfig", menuName = "ScriptableObjects/GameConfig", order = 0)]
     public class GameConfig : ScriptableObject
     {
-        public float tomatoTreeGrowTime = 0.2f;
+        public float tomatoTreeGrowTime = 1f;
         public float tomatoMoveTime = 0.5f;
         public float boxScaleTime = 0.3f;
     }
